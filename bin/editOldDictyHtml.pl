b28d900667ccd67a53c8f3703107c22f1c35c534
David Jimenez-Morales <biodavidjm@gmail.com>
2015-06-30 14:42:53 -0500

    Script to process old dicty html pages
    It will parse the old html files and make general changes to be part of
    AngularJS views

---
+++ bin/editOldDictyHtml.pl
@@ -0,0 +1,97 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+use feature qw/say/;
+
+use IO::File;
+use autodie qw/open close/;
+
+# Deal with files
+use FindBin;
+use File::Path qw( make_path );
+
+# Validation section
+my $script = "editOldDictyHtml.pl";
+if ( @ARGV != 1 ) {
+    print "One ARGUMENT NEEDED ----->";
+    say "Usage: $script filename.html";
+    exit;
+}
+
+# Input file
+my $filename = $ARGV[0];
+die "---> Hey!!! "
+    . $filename
+    . " does not have the 'html' extension. Why not?\n"
+    if ( $filename !~ /.*\.html/ );
+
+my $output_filename = "ajs-".$filename;
+
+open my $in_fh, '<', $filename
+    or die "Sorry, but I cannot open '$filename'!\n";
+
+open my $out_fh, '+>', $output_filename or die $!;
+
+my $count = 0;
+my $flag_head = 0;
+
+# # Mapping systematic name to many Associated Genes
+while ( my $line = <$in_fh> ) {
+    chomp($line);
+    if ( $line =~ /^\s{0,}<!DOCTYPE/ ) 
+    { 
+        say "DELETE: " . $line; 
+        next; 
+    }
+    if ($line =~ /^\s{0,}<head/ ) 
+    {
+        say "#".$line;
+        $flag_head = 1;
+        next;
+    }
+    if ( ($flag_head) && ($line !~ /^\s{0,}<\/head/ ) )
+    {
+        say "#".$line;
+    }
+    if ( ($flag_head) && ($line =~ /^\s{0,}<\/head/ ) )
+    {
+        say "#".$line;
+        $flag_head = 0; 
+    }
+}
+
+exit;
+
+sub get_path {
+    my @tries = @_;
+    my $good_path;
+    for my $try_path ( @tries ) {
+    if( -e $try_path and -d _ and -w _ ) {          # Path exists. Done.
+      $good_path = $try_path;
+      last;
+    }
+    elsif( eval { make_path( $try_path ); 1; } ) {  # Try to create it.
+      $good_path = $try_path;                       # Success, we're done.
+      last;
+    }
+    }
+    # Failure; fall through to next iteration.  
+    # If no more options, loop ends with $path undefined.
+    return $good_path;
+}
+
+=head1 NAME
+
+editOldDictyHtml.pl - Edit old dictyBase html files to adapt them to the views in AngularJS
+
+=head1 SYNOPSIS
+
+perl editOldDictyHtml.pl filename.html
+
+=head1 OUTPUT
+
+processed/filename.html Process the files to a new folder
+
+=head1 DESCRIPTION 
+
